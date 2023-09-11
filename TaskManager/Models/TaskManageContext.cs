33907cbd1953badbb781e883b7b530758274defb
Alexey <alexey.saulin03@mail.ru>
2023-09-11 16:12:01 +0300

    Removed unnecessary entity

--- TaskManager/Models/TaskManageContext.cs
+++ TaskManager/Models/TaskManageContext.cs
@@ -6,11 +6,9 @@ namespace TaskManager.Models;
 public class TaskManageContext : DbContext
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
-        optionsBuilder.UseSqlite($"Data Source=TaskManager.db");
+        optionsBuilder.UseSqlite("Data Source=TaskManager.db");
 
     public DbSet<Task> Tasks { get; set; }
     public DbSet<Tag> Tags { get; set; }
     public DbSet<Reminder> Reminders { get; set; }
-    public DbSet<SyncService> SyncServices { get; set; }
-    public DbSet<User> Users { get; set; }
 }
