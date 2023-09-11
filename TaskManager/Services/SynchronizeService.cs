058b30b7139a2b6c105b534fa74690c4fccd6be9
Alexey <alexey.saulin03@mail.ru>
2023-09-11 16:49:30 +0300

    Added ipInfo json class Added GetCurrentIp method receive from ipinfo io json

--- TaskManager/Services/SynchronizeService.cs
+++ TaskManager/Services/SynchronizeService.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 using TaskManager.Models;
 
 namespace TaskManager.Services;
@@ -15,4 +18,19 @@ public static class SynchronizeService
     {
         return default;
     }
+
+    public static async Task<IpInfo?> GetCurrentIpAsync()
+    {
+        using var client = new HttpClient();
+        var response = await client.GetAsync("https://ipinfo.io/json");
+
+        if (!response.IsSuccessStatusCode) return default;
+        
+        var jsonContent = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<IpInfo>(jsonContent);
+
+
+    }
+    
+    
 }
