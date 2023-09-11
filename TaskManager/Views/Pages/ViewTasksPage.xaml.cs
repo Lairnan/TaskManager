01d194d18c36fc37ac9ee376382fdc077c09384f
Alexey <alexey.saulin03@mail.ru>
2023-09-11 18:27:33 +0300

    Added main style button Added dependency injection Added gear png

---
+++ TaskManager/Views/Pages/ViewTasksPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TaskManager.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ViewTasksPage.xaml
+    /// </summary>
+    public partial class ViewTasksPage : Page
+    {
+        public ViewTasksPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
