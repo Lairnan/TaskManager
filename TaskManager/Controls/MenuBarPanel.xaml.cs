1e364df862a1e13e5438ec3c14f0dd4212fe9caf
Alexey <alexey.saulin03@mail.ru>
2023-09-13 19:24:26 +0300

    Rebuilded menu bar with code behind Removed MenuViewModel Removed TestCommand from MainViewModel

---
+++ TaskManager/Controls/MenuBarPanel.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interop;
+
+namespace TaskManager.Controls
+{
+    public partial class MenuBarPanel : UserControl
+    {
+        public MenuBarPanel()
+        {
+            InitializeComponent();
+            Loaded += (s, e) =>
+            {
+                var window = Window.GetWindow(this);
+                if (window == null) return;
+                
+                window.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+                SwitchStateBtn.Visibility = window.ResizeMode != ResizeMode.NoResize
+                    ? Visibility.Visible
+                    : Visibility.Collapsed;
+                SwitchStateBtn.Content = window.WindowState == WindowState.Maximized ? "2" : "1";
+            };
+        }
+        
+        public object Header
+        {
+            get => GetValue(HeaderProperty);
+            set => SetValue(HeaderProperty, value);
+        }
+
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register(nameof(Header), typeof(object), typeof(MenuBarPanel), new UIPropertyMetadata(null, OnHeaderChanged));
+
+        private static void OnHeaderChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is MenuBarPanel panel) panel.HeaderPresenter.Content = e.NewValue;
+        }
+
+        public new object Content
+        {
+            get => GetValue(ContentProperty);
+            set => SetValue(ContentProperty, value);
+        }
+
+        public new static readonly DependencyProperty ContentProperty =
+            DependencyProperty.Register(nameof(Content), typeof(object), typeof(MenuBarPanel), new UIPropertyMetadata(null, OnContentChanged));
+
+        private static void OnContentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is MenuBarPanel panel) panel.ContentPresenter.Content = e.NewValue;
+        }
+        
+        
+        [DllImport("user32.dll")]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void PnlControlBar_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount == 2)
+            {
+                SwitchState();
+                return;
+            }
+
+            var window = Window.GetWindow(this);
+            if (window == null) return;
+            
+            var helper = new WindowInteropHelper(window);
+            SwitchStateBtn.Content = window.WindowState == WindowState.Maximized ? "2" : "1";
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+
+        private void PnlControlBar_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            var window = Window.GetWindow(this);
+            if (window != null) window.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var window = Window.GetWindow(this);
+            window?.Close();
+        }
+
+        private void MinimizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var window = Window.GetWindow(this);
+            if (window != null) window.WindowState = WindowState.Minimized;
+        }
+
+        private void SwitchStateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SwitchState();
+        }
+
+        private void SwitchState()
+        {
+            var window = Window.GetWindow(this);
+            if (window == null) return;
+            
+            window.WindowState =
+                window.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+            SwitchStateBtn.Content = window.WindowState == WindowState.Maximized ? "2" : "1";
+        }
+    }
+}
--- TaskManager/ViewModels/MainViewModel.cs
+++ TaskManager/ViewModels/MainViewModel.cs
@@ -7,37 +7,19 @@ using TaskManager.Models;
 
 namespace TaskManager.ViewModels;
 
-public class MainViewModel : BindableBase, ISingleton
+public class MainViewModel : BindableBase, ITransient
 {
     private readonly TaskManageContext _taskManageContext;
 
-    public ICommand? ShowSettingsCommand { get; }
-    public ICommand? TestCommand { get; }
+    public ICommand ShowSettingsCommand => new DelegateCommand(ShowSetting);
     
     public MainViewModel(TaskManageContext taskManageContext)
     {
         _taskManageContext = taskManageContext;
-
-        ShowSettingsCommand = new DelegateCommand(ShowSetting);
-        TestCommand = new DelegateCommand(Test);
-        //TODO: Команды не работают, доделать
     }
 
     private void ShowSetting()
     {
         MessageBox.Show("Work In Progress");
-    }
-
-    private async void Test()
-    {
-        if (_taskManageContext.Tags.Any())
-            MessageBox.Show(_taskManageContext.Tags.First().Name);
-        else
-        {
-            var tag = new Models.Entities.Tag { Name = "New Tag" };
-            await _taskManageContext.AddAsync(tag);
-            await _taskManageContext.SaveChangesAsync();
-            MessageBox.Show(_taskManageContext.Tags.First().Name);
-        }
     }
 }
