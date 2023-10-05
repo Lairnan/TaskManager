0681042c51b94e9a7943ad56245d843ecae1010c
Alexey <alexey.saulin03@mail.ru>
2023-10-05 12:44:37 +0300

    Added ignore tests

--- TestProject.Test.E2E/UnitTest1.cs
+++ TestProject.Test.E2E/UnitTest1.cs
@@ -6,38 +6,89 @@ namespace TestProject.Test.E2E;
 public class Tests
 {
     private readonly string _desktopDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+    private WindowsDriver<WindowsElement> _driver = null!;
     
-    [SetUp]
+    [OneTimeSetUp]
     public void Setup()
-    {
-    }
-
-    [Test]
-    public void Test1()
     {
         var options = new AppiumOptions();
         options.AddAdditionalCapability("app", @$"{_desktopDirectory}\TaskManager\TaskManager\bin\Debug\net7.0-windows\TaskManager.exe");
         options.AddAdditionalCapability("appWorkingDir", @$"{_desktopDirectory}\TaskManager\TaskManager\bin\Debug\net7.0-windows");
         
-        var driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723/"),
+        _driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723/"),
             options);
         
+        Thread.Sleep(1000);
+    }
+
+    [OneTimeTearDown]
+    public void TearDown()
+    {
+        _driver.Close();
+    }
+    
+    [Test]
+    public void ClearTagsButtonIsEnabledTest()
+    {
         Thread.Sleep(500);
 
-        try
-        {
-            driver.FindElementByAccessibilityId("AddTagToFilterButton").Click();
-            driver.FindElementByAccessibilityId("RemoveTagFromFilterButton").Click();
+        _driver.FindElementByAccessibilityId("AddTagToFilterButton").Click();
+        
+        _driver.FindElementByAccessibilityId("RemoveTagFromFilterButton").Click();
 
-            driver.FindElementByAccessibilityId("AddTagToFilterButton").Click();
-            var clearTag = driver.FindElementByAccessibilityId("ClearTagsButton");
-            Assert.That(clearTag.Enabled, Is.EqualTo(true));
-            clearTag.Click();
-            Assert.That(clearTag.Enabled, Is.EqualTo(false));
-        }
-        finally
-        {
-            driver.Close();
-        }
+        _driver.FindElementByAccessibilityId("AddTagToFilterButton").Click();
+        var clearTag = _driver.FindElementByAccessibilityId("ClearTagsButton");
+        Assert.That(clearTag.Enabled, Is.EqualTo(true));
+        clearTag.Click();
+    }
+
+    // TODO: Не определяется количество элементов в ItemsControl.
+    [Ignore("Always empty")]
+    [Test]
+    public void HasCollectionInFilterTagsTest()
+    {
+        Thread.Sleep(500);
+
+        _driver.FindElementByAccessibilityId("AddTagToFilterButton").Click();
+
+        var filter = _driver.FindElementsByAccessibilityId("FilterTagsItemsControl");
+        Assert.That(filter, Is.Not.Empty);
+        _driver.FindElementByAccessibilityId("ClearTagsButton").Click();
+    }
+
+    // TODO: Не выполняется правильно команда Enter, после ввода элемента.
+    [Ignore("Enter not working")]
+    [Test]
+    public void ComboBoxEditableAddedTagsToFilterTest()
+    {
+        Thread.Sleep(500);
+
+        var tagsCombobox = _driver.FindElementByAccessibilityId("TagsCollectionComboBox");
+        tagsCombobox.SendKeys("New Tag");
+        tagsCombobox.Submit();
+
+        var filter = _driver.FindElementsByAccessibilityId("FilterTagsItemsControl");
+        Assert.That(filter, Is.Not.Empty);
+        _driver.FindElementByAccessibilityId("ClearTagsButton").Click();
+    }
+
+    // TODO: При открытии combobox, не получает элементы внутри combobox.
+    [Ignore("Enter not working")]
+    [Test]
+    public void ComboBoxClickableElementTest()
+    {
+        Thread.Sleep(500);
+        
+        var tagsCombobox = _driver.FindElementByAccessibilityId("TagsCollectionComboBox");
+        tagsCombobox.Click();
+        Thread.Sleep(150);
+        var tags = tagsCombobox.FindElementsById("ComboBoxItem");
+        tags.First().Click();
+
+        _driver.FindElementByAccessibilityId("AddFilterFromComboBoxButton").Click();
+        
+        var filter = _driver.FindElementsByAccessibilityId("FilterTagsItemsControl");
+        Assert.That(filter, Is.Not.Empty);
+        _driver.FindElementByAccessibilityId("ClearTagsButton").Click();
     }
 }
